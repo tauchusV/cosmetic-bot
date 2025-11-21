ad29ab846533ac534badd09bdab1c570bfb19ca1
tauchusV <anagornakova09@gmail.com>
2025-11-21 22:02:52 +0300

    config

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "8482039207:AAEOxj7vjwO_0L9IWZF80PvAWqky5RRj8zc"  # от @BotFather
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+# Админ-аккаунт студии (username БЕЗ @)
+ADMIN_USERNAME = "vivi_hairADM"
